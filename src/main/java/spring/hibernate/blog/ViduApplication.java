4e1e1dda0b5ac94751d1a0744900016dd5be3efd
Nguyễn Minh Lộc <Nguyn Minh Lc>
2016-10-18 12:23:05 +0700

    Signed-off-by: Nguyễn Minh Lộc <Nguyễn Minh Lộc>

---
+++ src/main/java/spring/hibernate/blog/ViduApplication.java
@@ -0,0 +1,12 @@
+package spring.hibernate.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViduApplication.class, args);
+	}
+}
